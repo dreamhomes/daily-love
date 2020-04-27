d8d3dcdcc3fea6d54b0414bc34582813e1d518e5
dreamhomes <2284672469@qq.com>
2020-04-27 21:21:50 +0800

    time format

--- main.py
+++ main.py
@@ -23,27 +23,27 @@ parser = argparse.ArgumentParser(description="*** Input parameters ***")
 parser.add_argument('-s', '--sender', type=str, help='Sender')
 parser.add_argument('-r', '--recipient', type=str, help='recipient')
 parser.add_argument('-c', '--city', default="101240214", type=str, help='city code')
-parser.add_argument('-t', '--time', default="8:00", type=str, help='reminder time')
+parser.add_argument('-t', '--time', default="08:00", type=str, help='reminder time')
 args = parser.parse_args()
-# print(args.sender, args.recipient, args.city, args.time)
+print(args.sender, args.recipient, args.city, args.time)
 
-xxx = "来自 @"+args.sender+" 的爱，么么哒：\n\n"
-location = "http://www.weather.com.cn/weather/" + args.city + ".shtml"
-
-name = itchat.search_friends(nickName=args.recipient)[0]['UserName']
-
-
-def main():
-    message = xxx + get_date() + get_daily_love() + get_information(location)
-    print('The message sending time:', datetime.datetime.now())
-    print(message)
-    # test
-    # itchat.send_msg(msg=message, toUserName='filehelper')
-    itchat.send_msg(msg=message, toUserName=name)
-
-
-# schedule.every(60).seconds.do(main)
-schedule.every().day.at(args.time).do(main)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# xxx = "来自 @"+args.sender+" 的爱，么么哒：\n\n"
+# location = "http://www.weather.com.cn/weather/" + args.city + ".shtml"
+#
+# name = itchat.search_friends(nickName=args.recipient)[0]['UserName']
+#
+#
+# def main():
+#     message = xxx + get_date() + get_daily_love() + get_information(location)
+#     print('The message sending time:', datetime.datetime.now())
+#     print(message)
+#     # test
+#     # itchat.send_msg(msg=message, toUserName='filehelper')
+#     itchat.send_msg(msg=message, toUserName=name)
+#
+#
+# # schedule.every(60).seconds.do(main)
+# schedule.every().day.at(args.time).do(main)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
